a8efb1a7d317d0eabe309af891b835d572fe96a5
Xavier Tondelier <xtondelier@gmail.com>
2025-10-23 17:57:12 +0100

    Add style to book display

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const yearInput = document.querySelector("#year");
 const readInput = document.querySelector("#read");
+const inputs = [titleInput, authorInput, pagesInput, yearInput, readInput]
 
 document.querySelector("#year").setAttribute("max", new Date().getFullYear());
 
@@ -19,8 +20,16 @@ function Book(title, author, pages, year, read) {
   };
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.year = year;
+  if (pages == "") {
+    this.pages = "unprovided";
+  } else {
+    this.pages = pages;
+  };
+  if (year == "") {
+    this.year = "unprovided";
+  } else {
+    this.year = year;
+  };
   this.read = read;
   this.id = crypto.randomUUID();
 };
@@ -31,15 +40,55 @@ function addBookToLibrary(title, author, pages, year, read) {
 };
 
 function addBookToPage(book) {
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("book-header");
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("book-content");
+  const topContentDiv = document.createElement("div");
+  const bottomContentDiv = document.createElement("div");
+
   const bookDisplay = document.createElement("div");
   bookDisplay.classList.add("book");
 
-  for (let key in book) {
-    const part = document.createElement("p");
-    part.textContent = book[key]
-    part.classList.add(key);
-    bookDisplay.appendChild(part);
-  };
+  const titleDisplay = document.createElement("h2");
+  titleDisplay.textContent = book.title;
+  titleDisplay.classList.add("title");
+  headerDiv.appendChild(titleDisplay);
+
+  const authorDisplay = document.createElement("p");
+  authorDisplay.textContent = `by ${book.author}`;
+  authorDisplay.classList.add("author");
+  headerDiv.appendChild(authorDisplay);
+
+  if (book.pages != "unprovided") {
+    const pagesDisplay = document.createElement("p");
+    pagesDisplay.textContent = `${book.pages} pages`;
+    pagesDisplay.classList.add("pages");
+    bottomContentDiv.appendChild(pagesDisplay);
+  }
+
+  if (book.year != "unprovided") {
+    const yearDisplay = document.createElement("p");
+    yearDisplay.textContent = `Released in ${book.year}`;
+    yearDisplay.classList.add("year");
+    bottomContentDiv.appendChild(yearDisplay);
+  }
+
+  const readDisplay = document.createElement("p");
+  readDisplay.textContent = `Status: ${(book.read) ? "Read" : "Not read yet"}`;
+  readDisplay.classList.add("read");
+  topContentDiv.appendChild(readDisplay);
+
+  const idDisplay = document.createElement("p");
+  idDisplay.textContent = `Id: ${book.id}`;
+  idDisplay.classList.add("id");
+  topContentDiv.appendChild(idDisplay);
+
+  [bottomContentDiv, topContentDiv].forEach(div => contentDiv.appendChild(div));
+  [headerDiv, contentDiv].forEach(div => bookDisplay.appendChild(div));
+
+  bookDisplay.style.backgroundImage = `url("images/book-cover${Math.floor(Math.random() * 3)}.jpg")`;
+  bookDisplay.style.backgroundSize = "cover";
 
   booksDisplay.appendChild(bookDisplay);
 };
@@ -63,7 +112,7 @@ submitBtn.addEventListener("click", evt => {
   evt.preventDefault();
 
   if (document.querySelectorAll(":invalid").length == 0) {
-    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, yearInput.value, readInput.value);
+    addBookToLibrary(...inputs.slice(0, -1).map(input => input.value), readInput.checked);
     addBookDialog.close();
   } else {
     submitBtn.style.color = "#F44";
